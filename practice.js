b6b7af78f8e9b323de4ca9c5408e8ef6f1f9c6bf
yechanpark <yechanpark07@gmail.com>
2024-04-23 22:53:49 -0700

    among_us

---
+++ practice.js
@@ -0,0 +1,16 @@
+let x = 'super awesome website'
+console.log('thanks '+x)
+function amongus(among) {
+    console.log('among');
+    console.log('us');
+    console.log(among)
+}
+const words = ['the','a','asdo','hi']
+words.forEach(amongus)
+for (word of ['asdfasdfasd','HIIIII','nah']) {
+    amongus(word);
+
+}
+if (true) {
+    console.log('TRUE>')
+}
